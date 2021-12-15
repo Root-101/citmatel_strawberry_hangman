1e6e1e04eea8e01cfa443f5ac3f73cb61ca27714
Jessica Aidyl <jgarciaalbalah@gmail.com>
2021-12-14 21:08:11 -0500

    HangManSubLevelControllerImpl init

--- lib/src/app/c_usecase_def/hangman_usecase_exporter.dart
+++ lib/src/app/c_usecase_def/hangman_usecase_exporter.dart
@@ -2,3 +2,4 @@ library hangman_usecase_exporter;
 
 //exporta los usecase def
 export 'hangman_level_usecase.dart';
+export 'hangman_sublevel_usecase.dart';
--- lib/src/ui/c_controller_impl/hangman_sub_level_controller_impl.dart
+++ lib/src/ui/c_controller_impl/hangman_sub_level_controller_impl.dart
@@ -1,9 +1,13 @@
 import 'package:citmatel_strawberry_hangman/hangman_exporter.dart';
-import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 
 class HangManSubLevelControllerImpl extends HangManSubLevelController {
-  HangManSubLevelDomain subLevelDomain;
+  late final HangManSubLevelUseCase subLevelUseCase;
 
-  HangManSubLevelControllerImpl({required this.subLevelDomain});
+  HangManSubLevelControllerImpl({
+    required HangManSubLevelDomain subLevelDomain,
+  }) : subLevelUseCase = HangManSubLevelUseCaseImpl(
+          subLevelDomain: subLevelDomain,
+        );
+        
+  HangManSubLevelDomain get subLevelDomain => subLevelUseCase.subLevelDomain;
 }
