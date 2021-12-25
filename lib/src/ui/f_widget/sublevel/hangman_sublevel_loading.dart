ff1e8c5a36eac0f08ef83b95cc94537654917e08
Jesus Hdez <jhernandezb96@gmail.com>
2021-12-25 11:31:11 -0500

    HangManSubLevelLoading: +loading & +_loadingWidget()

--- lib/src/ui/f_widget/sublevel/hangman_sublevel_loading.dart
+++ lib/src/ui/f_widget/sublevel/hangman_sublevel_loading.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 class HangManSubLevelLoading extends StatelessWidget {
   static const int DURATION = 3; //loading timeout
@@ -9,11 +10,32 @@ class HangManSubLevelLoading extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: _loading(),
+      builder: (context, AsyncSnapshot snapshot) {
+        // Show splash screen while waiting for app resources to load:
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return _loadingWidget();
+        } else {
+          onEnd.call();
+          return Container(); //hay que devolver algo
+        }
+      },
+    );
+  }
+
+  _loading() async {
+    await Future.delayed(Duration(seconds: HangManSubLevelLoading.DURATION));
+  }
+
+  _loadingWidget() {
     return Container(
+      padding: EdgeInsets.all(30.0),
       child: Column(
         mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: [
-          Text("Comenzando el nivel en..."),
+          Text("Cargando nivel"),
+          const SpinKitWave(color: Colors.white, type: SpinKitWaveType.start),
         ],
       ),
     );
