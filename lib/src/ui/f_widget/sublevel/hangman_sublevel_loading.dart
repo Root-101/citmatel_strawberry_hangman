c94770ae9a4b407343d2c8173176b88aa3fe97cc
Jesus Hdez <jhernandezb96@gmail.com>
2021-12-25 11:48:34 -0500

    HangManSubLevelLoading: Random loading animation

--- lib/src/ui/f_widget/sublevel/hangman_sublevel_loading.dart
+++ lib/src/ui/f_widget/sublevel/hangman_sublevel_loading.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 
@@ -6,7 +8,12 @@ class HangManSubLevelLoading extends StatelessWidget {
 
   final VoidCallback onEnd;
 
-  HangManSubLevelLoading({Key? key, required this.onEnd}) : super(key: key);
+  //hay que controlarlo por indice, si se pone el random directo en el build() cambia la animacion cuando se da back o cosas similares
+  int _kitIndex = 0;
+
+  HangManSubLevelLoading({Key? key, required this.onEnd}) : super(key: key) {
+    _kitIndex = Random().nextInt(kits.length);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -35,9 +42,50 @@ class HangManSubLevelLoading extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: [
           Text("Cargando nivel"),
-          const SpinKitWave(color: Colors.white, type: SpinKitWaveType.start),
+          kits[_kitIndex], //a random loading every time
         ],
       ),
     );
   }
+
+  late final kits = <Widget>[
+    const SpinKitRotatingCircle(color: Colors.white),
+    const SpinKitRotatingPlain(color: Colors.white),
+    const SpinKitChasingDots(color: Colors.white),
+    const SpinKitPumpingHeart(color: Colors.white),
+    const SpinKitPulse(color: Colors.white),
+    const SpinKitDoubleBounce(color: Colors.white),
+    const SpinKitWave(color: Colors.white, type: SpinKitWaveType.start),
+    const SpinKitWave(color: Colors.white, type: SpinKitWaveType.center),
+    const SpinKitWave(color: Colors.white, type: SpinKitWaveType.end),
+    const SpinKitPianoWave(
+        color: Colors.white, type: SpinKitPianoWaveType.start),
+    const SpinKitPianoWave(
+        color: Colors.white, type: SpinKitPianoWaveType.center),
+    const SpinKitPianoWave(color: Colors.white, type: SpinKitPianoWaveType.end),
+    const SpinKitThreeBounce(color: Colors.white),
+    const SpinKitThreeInOut(color: Colors.white),
+    const SpinKitWanderingCubes(color: Colors.white),
+    const SpinKitWanderingCubes(color: Colors.white, shape: BoxShape.circle),
+    const SpinKitCircle(color: Colors.white),
+    const SpinKitFadingFour(color: Colors.white),
+    const SpinKitFadingFour(color: Colors.white, shape: BoxShape.rectangle),
+    const SpinKitFadingCube(color: Colors.white),
+    const SpinKitCubeGrid(size: 51.0, color: Colors.white),
+    const SpinKitFoldingCube(color: Colors.white),
+    const SpinKitRing(color: Colors.white),
+    const SpinKitDualRing(color: Colors.white),
+    const SpinKitSpinningLines(color: Colors.white),
+    const SpinKitFadingGrid(color: Colors.white),
+    const SpinKitFadingGrid(color: Colors.white, shape: BoxShape.rectangle),
+    const SpinKitSquareCircle(color: Colors.white),
+    const SpinKitSpinningCircle(color: Colors.white),
+    const SpinKitSpinningCircle(color: Colors.white, shape: BoxShape.rectangle),
+    const SpinKitFadingCircle(color: Colors.white),
+    const SpinKitHourGlass(color: Colors.white),
+    const SpinKitPouringHourGlass(color: Colors.white),
+    const SpinKitPouringHourGlassRefined(color: Colors.white),
+    const SpinKitRipple(color: Colors.white),
+    const SpinKitDancingSquare(color: Colors.white),
+  ];
 }
