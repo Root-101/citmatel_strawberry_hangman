43096cd68bf88a0d6114a8000ec4121f779fe86e
Jesus Hdez <jhernandezb96@gmail.com>
2021-12-25 11:31:25 -0500

    HangManSubLevelBackground: +background

--- lib/src/ui/f_widget/sublevel/hangman_sublevel_background.dart
+++ lib/src/ui/f_widget/sublevel/hangman_sublevel_background.dart
@@ -2,14 +2,14 @@ import 'package:citmatel_strawberry_hangman/hangman_exporter.dart';
 import 'package:flutter/material.dart';
 
 class HangManSubLevelBackground extends StatefulWidget {
-  //TODO: cambiar el nombre ese xds
   final HangManSubLevelDomain subLevelDomain;
 
   const HangManSubLevelBackground({required this.subLevelDomain, Key? key})
       : super(key: key);
 
   @override
-  _HangManSubLevelBackgroundState createState() => _HangManSubLevelBackgroundState();
+  _HangManSubLevelBackgroundState createState() =>
+      _HangManSubLevelBackgroundState();
 }
 
 class _HangManSubLevelBackgroundState extends State<HangManSubLevelBackground> {
@@ -36,21 +36,16 @@ class _HangManSubLevelBackgroundState extends State<HangManSubLevelBackground> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      decoration: new BoxDecoration(
+      decoration: BoxDecoration(
         color: Colors.transparent, //pa si por si acaso
-        image: new DecorationImage(
-          //TODO: @Aidyl98 genial el uso de la imagen de fondo en el container, asi nos quitamos usar el stack para background de imagen, GENIAL!!!!
-          image: new AssetImage("assets/icons/background.jpg"),
+        image: DecorationImage(
+          image: AssetImage("assets/icons/hangman_wallpaper.jpg"),
           fit: BoxFit.cover,
         ),
       ),
       child: AnimatedSwitcher(
         duration: const Duration(milliseconds: 500),
         transitionBuilder: (Widget child, Animation<double> animation) {
-          /*return RotationTransition(//TODO: @Aidyl98 elige que transición te gusta mas
-            turns: animation,
-            child: child,
-          );*/
           return ScaleTransition(
             child: child,
             scale: animation,
