2a8e4775ef541d19c2f1d2e363708a05313204f1
Aidyl <jgarciaalbalah@gmail.com>
2022-01-05 10:58:15 -0500

    HangManSubLevelWin: +customize

--- lib/src/ui/f_widget/sublevel/hangman_sublevel_win.dart
+++ lib/src/ui/f_widget/sublevel/hangman_sublevel_win.dart
@@ -1,4 +1,5 @@
 import 'package:animated_background/animated_background.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
 
 class HangManSubLevelWin extends StatefulWidget {
@@ -14,15 +15,67 @@ class _HangManSubLevelWinState extends State<HangManSubLevelWin>
     with TickerProviderStateMixin {
   @override
   Widget build(BuildContext context) {
+    const colorizeColors = [
+      Colors.purple,
+      Colors.blue,
+      Colors.yellow,
+      Colors.red,
+    ];
+
+    const colorizeTextStyle = TextStyle(
+      fontSize: 70.0,
+      fontFamily: 'Horizon',
+    );
     return Scaffold(
       body: AnimatedBackground(
         behaviour: RandomParticleBehaviour(
           options: ParticleOptions(
-            baseColor: Colors.red,
+            baseColor: Colors.blueAccent,
           ),
         ),
         vsync: this,
-        child: Text('You Win!'),
+        child: AnimatedBackground(
+          behaviour: RandomParticleBehaviour(
+            options: ParticleOptions(
+              baseColor: Colors.red,
+            ),
+          ),
+          vsync: this,
+          child: Container(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                SizedBox(
+                  child: AnimatedTextKit(
+                    animatedTexts: [
+                      ColorizeAnimatedText(
+                        'Felicidades',
+                        textStyle: colorizeTextStyle,
+                        colors: colorizeColors,
+                      ),
+                    ],
+                    repeatForever: true,
+                  ),
+                ),
+                DefaultTextStyle(
+                  style: const TextStyle(
+                    fontSize: 50.0,
+                    color: Colors.blueAccent,
+                  ),
+                  child: AnimatedTextKit(
+                    animatedTexts: [
+                      RotateAnimatedText('Has Ganado'),
+                      RotateAnimatedText('Lo Lograste'),
+                      RotateAnimatedText('Eres el Mejor'),
+                    ],
+                    repeatForever: true,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
