b9638574aa059ecfc239fef462988d67534d0a9e
Aidyl <jgarciaalbalah@gmail.com>
2022-01-07 14:49:41 -0500

    +HangManAssets.dart

--- lib/hangman_exporter.dart
+++ lib/hangman_exporter.dart
@@ -3,3 +3,4 @@ library hangman_exporter;
 //export all the hangman module
 export 'src/app/hangman_app_exporter.dart';
 export 'src/ui/hangman_ui_exporter.dart';
+export 'assets/hangman_assets.dart';
