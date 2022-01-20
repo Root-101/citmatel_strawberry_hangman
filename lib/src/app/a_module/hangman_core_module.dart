82ef6b6e7db3c30952fdb0d2e40c690354d374e4
JHB <jhernandezb96@gmail.com>
2022-01-20 17:17:59 -0500

    HangManCoreModule: fix to Future of repo

--- lib/src/app/a_module/hangman_core_module.dart
+++ lib/src/app/a_module/hangman_core_module.dart
@@ -1,8 +1,25 @@
-import 'package:citmatel_strawberry_hangman/src/app/hangman_app_exporter.dart';
+import 'package:citmatel_strawberry_hangman/hangman_exporter.dart';
 import 'package:get/get.dart';
 
 class HangManCoreModule {
-  static void init() {
-    Get.put<HangManLevelUseCase>(HangManLevelUseCaseImpl(HangManLevelsAll.levels));
+  static Future init() async {
+    await HangManRepoModule.init().then((value) {
+      //sin dependencia
+      Get.put<HangManLevelUseCase>(
+          HangManLevelUseCaseImpl(HangManLevelsAll.levels));
+
+      //el de progreso con la BD
+      Get.put<HangManSubLevelProgressUseCase>(
+        HangManSubLevelProgressUseCaseImpl(
+            HangManRepoModule.subLevelProgressRepo),
+      );
+
+      return value;
+    });
+  }
+
+  static void dispose() {
+    HangManRepoModule.dispose();
+    Get.deleteAll(force: true);
   }
 }
