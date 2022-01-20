1a214f95a8cfb244b9abefe76c98dd91dabe2751
JHB <jhernandezb96@gmail.com>
2022-01-20 17:11:11 -0500

    fix exporter with new repo

--- lib/src/app/hangman_app_exporter.dart
+++ lib/src/app/hangman_app_exporter.dart
@@ -4,3 +4,4 @@ export 'a_module/hangman_core_module.dart';
 export 'b_domain/hangman_domain_exporter.dart';
 export 'c_usecase_def/hangman_usecase_exporter.dart';
 export 'd_usecase_impl/hangman_usecase_impl_exporter.dart';
+export 'e_repo_def/hangman_repodef_exporter.dart';
