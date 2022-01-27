04ff1794788c752f0a59e78fe7826c45cf81e2c3
Aidyl <jgarciaalbalah@gmail.com>
2022-01-27 04:38:48 -0500

    HangManLevel1: +data info

--- lib/src/app/b_domain/levels/hangman_level1.dart
+++ lib/src/app/b_domain/levels/hangman_level1.dart
@@ -1,21 +1,16 @@
 import 'package:citmatel_strawberry_hangman/hangman_exporter.dart';
 
 class HangManLevel1 {
-  static final HangManSubLevelDomain sublevel = HangManSubLevelDomain(
-    id: 1,
-    answer: "Hello",
-    urlImage: HangManAssets.LEVEL_1_EXAMPLE,
-  );
   static final HangManLevelDomain level1 = HangManLevelDomain(
     id: 1,
     theme: "Tutorial",
     urlThemePicture: HangManAssets.WALLPAPER,
     sublevel: [
-      sublevel.clone()..id = 1,
-      sublevel.clone()..id = 2,
-      sublevel.clone()..id = 3,
-      sublevel.clone()..id = 4,
-      sublevel.clone()..id = 5,
+      HangManSubLevelDomain(
+        id: 1,
+        answer: "Tutorial",
+        urlImage: HangManAssets.TUTORIAL,
+      ),
     ],
   );
 }
