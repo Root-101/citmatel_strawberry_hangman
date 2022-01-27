787e40f20d0a7aa673412d09b3f990963521e4a9
Aidyl <jgarciaalbalah@gmail.com>
2022-01-27 04:36:48 -0500

    HangManAssets: + Level 1 & 2

--- lib/assets/hangman_assets.dart
+++ lib/assets/hangman_assets.dart
@@ -1,7 +1,21 @@
 class HangManAssets {
   static const _assets = "packages/citmatel_strawberry_hangman/assets";
   static const _icons = "$_assets/icons";
+  static const _level1 = "$_assets/level1";
+  static const _level2 = "$_assets/level2";
 
   static const String WALLPAPER = "$_icons/hangman_wallpaper.jpg";
   static const String LEVEL_1_EXAMPLE = "$_icons/brain_in_blue.jpg";
+
+  static const String TUTORIAL = "$_level1/tutorial.jpg";
+
+  static const String CONFERENCIA = "$_level2/conferencia.png";
+  static const String ESCUELA = "$_level2/escuela.png";
+  static const String GRADUACION = "$_level2/graduacion.png";
+
+  static const String ESCRIBIR = "$_level2/escribir.jpg";
+  static const String EXAMEN = "$_level2/examen.jpg";
+  static const String LABORATORIO = "$_level2/laboratorio.jpg";
+  static const String LIBRO = "$_level2/libros.jpg";
+  static const String NOTAS = "$_level2/notas.jpg";
 }
