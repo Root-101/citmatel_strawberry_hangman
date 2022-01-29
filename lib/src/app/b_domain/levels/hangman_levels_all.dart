d726a8cdaaa52d9c9f0aa4bfbcc2283186ae6962
JHB <jhernandezb96@gmail.com>
2022-01-29 02:21:29 -0500

    +HangManLevelTutorial

--- lib/src/app/b_domain/levels/hangman_levels_all.dart
+++ lib/src/app/b_domain/levels/hangman_levels_all.dart
@@ -2,6 +2,7 @@ import 'package:citmatel_strawberry_hangman/src/app/hangman_app_exporter.dart';
 
 class HangManLevelsAll {
   static final List<HangManLevelDomain> levels = [
+    HangManLevelTutorial.tutorial,
     HangManLevel1.level1,
     HangManLevel2.level2,
   ];
