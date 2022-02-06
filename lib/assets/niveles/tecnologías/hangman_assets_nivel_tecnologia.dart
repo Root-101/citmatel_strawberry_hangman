838ee91ace09dddfb4fface958d9fad1b6d904d5
Aidyl <jgarciaalbalah@gmail.com>
2022-02-06 18:52:03 -0500

    + HangManAssetsNivelTecnologias

---
+++ lib/assets/niveles/tecnologías/hangman_assets_nivel_tecnologia.dart
@@ -0,0 +1,7 @@
+class HangManAssetsNivelTecnologias {
+  static const _assets = "packages/citmatel_strawberry_hangman/assets";
+  static const _niveles = "$_assets/niveles";
+  static const _tecnologias = "$_niveles/tecnologías";
+
+  static const String TECNOLOGIA_COHETE= "$_tecnologias/cohete.jpg";
+}
