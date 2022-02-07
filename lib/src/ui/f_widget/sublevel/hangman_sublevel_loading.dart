b2dc11d7b67d2b218f2695be82c672e54c0b76c7
Aidyl <jgarciaalbalah@gmail.com>
2022-02-07 18:39:40 -0500

    HangManSubLevelLoading: +themeOfGivenLevel

--- lib/src/ui/f_widget/sublevel/hangman_sublevel_loading.dart
+++ lib/src/ui/f_widget/sublevel/hangman_sublevel_loading.dart
@@ -1,8 +1,9 @@
 import 'package:citmatel_strawberry_hangman/hangman_exporter.dart';
 import 'package:citmatel_strawberry_tools/tools_exporter.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
-class HangManSubLevelLoading extends StatelessWidget {
+class HangManSubLevelLoading extends GetView<HangManLevelController> {
   final HangManSubLevelDomain subLevelDomain;
   final HangManSubLevelProgressDomain subLevelProgressDomain;
 
@@ -16,24 +17,13 @@ class HangManSubLevelLoading extends StatelessWidget {
   Widget build(BuildContext context) {
     return PlainSubLevelLoading(
       backgroundURL: HangManAssets.WALLPAPER,
-      loading: _loadingWidget(),
+      firstText: [
+        "Tema: ${controller.themeOfGivenLevel(subLevelProgressDomain)}"
+      ],
+      secondText: ["Nivel: ${subLevelProgressDomain.hangmanSubLevelDomainId}"],
       subLevel: HangManSubLevelScreen(
         subLevelDomain: subLevelDomain,
         subLevelProgressDomain: subLevelProgressDomain,
-      ),
-    );
-  }
-
-  _loadingWidget() {
-    return Container(
-      padding: EdgeInsets.all(30.0),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: [
-          Text("Cargando nivel"),
-          StrawberryLoadingSpinkit.randomLoadingSpinkit(),
-          //a random loading every time
-        ],
       ),
     );
   }
