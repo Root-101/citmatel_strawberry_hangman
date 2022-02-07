84a71fafa4cb856f88b28af7a55613fbafca4154
Aidyl <jgarciaalbalah@gmail.com>
2022-02-07 18:37:44 -0500

    HangManLevelUseCase & Impl: themeOfGivenLevel

--- lib/src/app/c_usecase_def/hangman_level_usecase.dart
+++ lib/src/app/c_usecase_def/hangman_level_usecase.dart
@@ -1,4 +1,6 @@
 import 'package:citmatel_strawberry_hangman/src/app/hangman_app_exporter.dart';
 import 'package:clean_core/clean_core.dart';
 
-abstract class HangManLevelUseCase extends ReadUseCase<HangManLevelDomain> {}
+abstract class HangManLevelUseCase extends ReadUseCase<HangManLevelDomain> {
+  String themeOfGivenLevel(HangManSubLevelProgressDomain progressDomain);
+}
