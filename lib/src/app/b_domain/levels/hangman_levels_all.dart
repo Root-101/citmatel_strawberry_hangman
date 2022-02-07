dd5d7a4241f7fcafcf2aed9c8f840a7a098971c8
Aidyl <jgarciaalbalah@gmail.com>
2022-02-06 19:10:08 -0500

    HangManLevelsAll & Exporter: new levels

--- lib/src/app/b_domain/levels/hangman_levels_all.dart
+++ lib/src/app/b_domain/levels/hangman_levels_all.dart
@@ -1,9 +1,11 @@
+import 'package:citmatel_strawberry_hangman/src/app/b_domain/hangman_domain_exporter.dart';
 import 'package:citmatel_strawberry_hangman/src/app/hangman_app_exporter.dart';
 
 class HangManLevelsAll {
   static final List<HangManLevelDomain> levels = [
     HangManLevelTutorial.tutorial,
-    HangManLevel1.level1,
-    HangManLevel2.level2,
+    HangManLevelConocimientoGeneral.levelConocimientoGeneral,
+    HangManLevelCienciasNaturales.levelCienciasNaturales,
+    HangManLevelTecnologias.levelTecnologias,
   ];
 }
