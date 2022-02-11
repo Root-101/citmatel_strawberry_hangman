9d8765c6818e07d706325b4265d875b373ab1570
Aidyl <jgarciaalbalah@gmail.com>
2022-02-11 00:20:34 -0500

    HangManAssetsNivelMitologia: +new assets

--- lib/assets/niveles/mitología/hangman_assets_nivel_mitologia.dart
+++ lib/assets/niveles/mitología/hangman_assets_nivel_mitologia.dart
@@ -3,7 +3,15 @@ class HangManAssetsNivelMitologia {
   static const _niveles = "$_assets/niveles";
   static const _mitologia = "$_niveles/mitología";
 
-  static const String MITOLOGIA_ANUBIS= "$_mitologia/anubis.png";
-  static const String MITOLOGIA_FARAON= "$_mitologia/faraón.jpg";
-  static const String MITOLOGIA_FENIX= "$_mitologia/fénix.jpg";
+  static const String MITOLOGIA_ANUBIS = "$_mitologia/anubis.png";
+  static const String MITOLOGIA_FARAON = "$_mitologia/faraón.jpg";
+  static const String MITOLOGIA_FENIX = "$_mitologia/fénix.jpg";
+  static const String MITOLOGIA_ARES = "$_mitologia/ares.jpg";
+  static const String MITOLOGIA_ARTEMISA = "$_mitologia/artemisa.jpg";
+  static const String MITOLOGIA_DEMETER = "$_mitologia/deméter.jpg";
+  static const String MITOLOGIA_FAUNO = "$_mitologia/fauno.jpg";
+  static const String MITOLOGIA_HADES = "$_mitologia/hades.jpg";
+  static const String MITOLOGIA_HEFESTO = "$_mitologia/hefesto.jpg";
+  static const String MITOLOGIA_POSEIDON = "$_mitologia/poseidón.jpg";
+  static const String MITOLOGIA_ZEUS = "$_mitologia/zeus.jpg";
 }
