5962ff3e813284e1fdd8b897a9eda8577de1c93a
JHB <jhernandezb96@gmail.com>
2022-02-12 23:13:51 -0500

    HangManUIModule & HangManLevelsScreen: +Generalized colors/name/icons

--- lib/src/ui/a_module/hangman_ui_module.dart
+++ lib/src/ui/a_module/hangman_ui_module.dart
@@ -1,8 +1,14 @@
-import 'package:citmatel_strawberry_hangman/src/app/hangman_app_exporter.dart';
-import 'package:citmatel_strawberry_hangman/src/ui/hangman_ui_exporter.dart';
+import 'package:citmatel_strawberry_hangman/hangman_exporter.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class HangManUIModule {
+  static const String MODULE_NAME = "Ahorcado";
+  static const Color PRIMARY_COLOR = Colors.blue;
+  static const Color SECONDARY_COLOR = Colors.lightBlueAccent;
+  static const String URL_MODULE_BACKGROUND = HangManAssets.WALLPAPER;
+  static const IconData ICON = Icons.sentiment_very_dissatisfied_outlined;
+
   static Future init() async {
     await HangManCoreModule.init();
 
