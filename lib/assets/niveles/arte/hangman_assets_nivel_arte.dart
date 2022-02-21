39446718ed5afecdf1ff3f96d353526ab0dd51c4
Aidyl <jgarciaalbalah@gmail.com>
2022-02-20 23:35:36 -0500

    HangManAssetsNivelArte: fix extensions

--- lib/assets/niveles/arte/hangman_assets_nivel_arte.dart
+++ lib/assets/niveles/arte/hangman_assets_nivel_arte.dart
@@ -5,7 +5,7 @@ class HangManAssetsNivelArte {
 
   static const String ARTE_1_PALETA = "$_arte/1-paleta.png";
   static const String ARTE_2_CANTANTE = "$_arte/2-cantante.png";
-  static const String ARTE_3_FOTOS = "$_arte/3-fotos.png";
+  static const String ARTE_3_FOTOS = "$_arte/3-fotos.jpg";
   static const String ARTE_4_DOMADOR = "$_arte/4-domador.png";
   static const String ARTE_5_PAYASO = "$_arte/5-payaso.png";
   static const String ARTE_6_FLAUTA = "$_arte/6-flauta.png";
@@ -13,16 +13,16 @@ class HangManAssetsNivelArte {
   static const String ARTE_8_VIOLIN = "$_arte/8-violín.png";
   static const String ARTE_9_BANDA = "$_arte/9-banda.png";
   static const String ARTE_10_COSTURA = "$_arte/10-costura.png";
-  static const String ARTE_11_DISENO = "$_arte/11-diseño.png";
+  static const String ARTE_11_DISENO = "$_arte/11-diseño.jpg";
   static const String ARTE_12_GUITARRA = "$_arte/12-guitarra.jpg";
-  static const String ARTE_13_MODELAR = "$_arte/13-modelar.png";
+  static const String ARTE_13_MODELAR = "$_arte/13-modelar.jpg";
   static const String ARTE_14_SAXOFON = "$_arte/14-saxofón.png";
   static const String ARTE_15_RESTAURAR = "$_arte/15-restaurar.png";
   static const String ARTE_16_TROMBON = "$_arte/16-trombón.png";
   static const String ARTE_17_CONCURSO = "$_arte/17-concurso.png";
   static const String ARTE_18_CONTRABAJO = "$_arte/18-contrabajo.png";
   static const String ARTE_19_CONCIERTO = "$_arte/19-concierto.png";
-  static const String ARTE_20_CORO = "$_arte/20-coro.png";
+  static const String ARTE_20_CORO = "$_arte/20-coro.jpg";
   static const String ARTE_21_VIOLA = "$_arte/21-viola.png";
   static const String ARTE_22_RAFAEL = "$_arte/22-rafael.png";
   static const String ARTE_23_ORQUESTA = "$_arte/23-orquesta.png";
